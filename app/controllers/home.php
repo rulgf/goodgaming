c050070279ff4b0f8646b27d1696de33a6a1460a
rulgf <rulgf.420@gmail.com>
2016-10-16 21:37:41 -0500

    Database ready

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,10 +1,17 @@
 <?php
 
 class Home extends Controller{
+
     public function index($name = ''){
         $user = $this->model('User');
-        $user->name = 'Alex';
-
+        $user->name = $name;
         $this->view('home/index', ['name' => $user->name]);
+    }
+
+    public function create($username = '', $email = ''){
+        User::create([
+           'name' => $username,
+            'email' => $email
+        ]);
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,9 @@
 <?php
 
-class User{
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent{
     public $name;
+
+    protected $fillable = ['name', 'email', 'password', 'age', 'city', 'country'];
 }

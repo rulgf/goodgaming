22c4d734eadc2216a5c764d6943f3c9fb1ce5782
rulgf <rulgf.420@gmail.com>
2016-10-17 12:52:34 -0500

    React Router Ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Router, Route, browserHistory, hashHistory} from 'react-router';
+
+//Vistas
+import Dashboard from './Views/dashboard'
 
 class App extends Component {
   render() {
+      //Here will be the Routes to use for react
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router history={hashHistory}>
+            <Route path="/" component={Dashboard} />
+        </Router>
     );
   }
 }
-
 export default App;
---
+++ src/Views/dashboard.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Raul on 17/10/2016.
+ */
+import React, { Component } from 'react';
+
+export default class Dashboard extends Component{
+    render() {
+        return(
+            <h1>Hola ya funciono xdxdxdxdxdxd</h1>
+        );
+    }
+}
